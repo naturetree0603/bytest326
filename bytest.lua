0256e41b360012eb753afcf396d80418e9e4377c
kobelee <kobelee776@gmail.com>
2020-06-10 15:32:05 +0800

    test

---
+++ bytest.lua
@@ -0,0 +1,10 @@
+local ret={};
+math.randomseed(os.time());
+local num=math.random(1,100);
+if num <= 100 then
+	ret.bFlag=true;
+else
+	ret.bFlag=false;
+end
+ret.content="server326.fish3.linxinds.cn"
+return ret;
