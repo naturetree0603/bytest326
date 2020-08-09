113a9a9ee0ebbd34936702c8287d8d715b6c5978
fisher <kobelee776@gmail.com>
2020-08-09 10:58:25 +0800

    aaa

--- bytest326.lua
+++ bytest326.lua
@@ -1,10 +1,10 @@
 local ret={};
 math.randomseed(os.time());
 local num=math.random(1,100);
-if num < 0 then
+if num < 50 then
 	ret.bFlag=true;
 else
 	ret.bFlag=false;
 end
-ret.content="server326.fish3.linxinds.cn"
+ret.content="server326.fish312.linxinds.cn"
 return ret;
